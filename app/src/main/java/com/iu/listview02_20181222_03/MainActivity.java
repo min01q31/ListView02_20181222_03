0a5d5c88694436fb516e651bbeb8d710a2a7f978
wosad878 <wosad878@naver.com>
2018-12-22 15:11:18 +0900

    어댑터와 액티비티 연결

--- app/src/main/java/com/iu/listview02_20181222_03/MainActivity.java
+++ app/src/main/java/com/iu/listview02_20181222_03/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.iu.listview02_20181222_03.adapters.StoreAdapter;
 import com.iu.listview02_20181222_03.datas.Store;
 
 import java.util.ArrayList;
@@ -12,7 +13,7 @@ import java.util.List;
 public class MainActivity extends BaseActivity {
     ListView storeListView;
     List<Store> storeList = new ArrayList<Store>();
-
+    StoreAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +31,21 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+        fillStores();
 
+        mAdapter = new StoreAdapter(mContext, storeList);
+        storeListView.setAdapter(mAdapter);
+    }
+    void fillStores() {
+        storeList.clear();
+
+        storeList.add(new Store("BBQ","종로구 10길",4,"오후2시부터 오전 1시","01012345678"));
+        storeList.add(new Store("BHC","종로구 11길",4,"오후2시부터 오전 3시","01045345678"));
+        storeList.add(new Store("네네치킨","종로구 12길",4,"오후6시부터 오전 11시","0101234678"));
+        storeList.add(new Store("굽네치킨","종로구 1길",4,"오후2시부터 오전 3시","01012975678"));
+        storeList.add(new Store("디디치킨","종로구 2길",4,"오후2시부터 오전 1시","01012745678"));
+        storeList.add(new Store("노랑통닭","종로구 5길",4,"오후2시부터 오전 1시","01012390128"));
+        storeList.add(new Store("교촌치킨","종로구 4길",4,"오후2시부터 오전 1시","01012975678"));
     }
 
     @Override
--- app/src/main/java/com/iu/listview02_20181222_03/adapters/StoreAdapter.java
+++ app/src/main/java/com/iu/listview02_20181222_03/adapters/StoreAdapter.java
@@ -2,7 +2,12 @@ package com.iu.listview02_20181222_03.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.iu.listview02_20181222_03.R;
 import com.iu.listview02_20181222_03.datas.Store;
@@ -23,5 +28,39 @@ public class StoreAdapter extends ArrayAdapter<Store> {
         this.inf = LayoutInflater.from(mContext);
     }
 
-    
+    //getView메쏘드는 리스트뷰가 한줄을 그리려고 할때 호출
+    //==>Ex 0번째 줄을 그리려고 하는데, 그 내용이 뭐니? 에 대한 답변을 작성
+    //몇번째 줄인지는 position에 담겨서 들어오게 됨.
+    //만약 100명의 회원? getView가 100번 호출=>리스트뷰는 필요한 만큼만 뷰를 생성하고,
+    // 그 외의 경우는 기존의 생성한 비률 재활용하도록 코딩
+    //뷰를 재활용
+    //-재활용이 아닌 상황이라면 convertView가 null이 담겨오게됨
+    //판단기준 : convertView가 null인지 아닌지
+    //row가 null일때 inflate를 실행, inflate? xml파일을 불러다가 실제로 메모리에 올려주는 역할
+
+    @Override
+    public View getView(int position,  View convertView,  ViewGroup parent) {
+        View row = convertView;
+        if(row == null){
+            row = inf.inflate(R.layout.store_list_item,null);
+        }
+        TextView storeNameTxt = row.findViewById(R.id.sotreNameTxt);
+        TextView storeAddressTxt = row.findViewById(R.id.storeAddressTxt);
+        Button storeCallBtn = row.findViewById(R.id.storeCallBtn);
+        TextView storeOpenTimeTxt = row.findViewById(R.id.storeOpenTimeTxt);
+
+        final Store data = mList.get(position);
+
+        storeNameTxt.setText(data.getName());
+        storeAddressTxt.setText(data.getAddress());
+        storeOpenTimeTxt.setText(data.getOpenAndCloseTime());
+
+        storeCallBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, data.getPhoneNum(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        return row;
+    }
 }
