26299929839a88f88de267f605bb91a8e19feb82
wosad878 <wosad878@naver.com>
2018-12-22 13:39:04 +0900

    Initial commit

---
+++ app/src/main/java/com/iu/listview02_20181222_03/MainActivity.java
@@ -0,0 +1,13 @@
+package com.iu.listview02_20181222_03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
